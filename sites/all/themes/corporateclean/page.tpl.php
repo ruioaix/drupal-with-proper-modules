d4f59b9e2b40117ecf6f6e0b03c859073ed41369
xrfind <xrfind@gmail.com>
2014-03-20 22:34:50 +0100

    theme corporateclean minor

--- sites/all/themes/corporateclean/page.tpl.php
+++ sites/all/themes/corporateclean/page.tpl.php
@@ -281,7 +281,7 @@
     <!-- #credits -->   
     <div id="credits" class="container_12 clearfix">
         <div class="grid_12">
-        <p>Ported to Drupal by <a href="http://www.drupalizing.com">Drupalizing</a>, a Project of <a href="http://www.morethanthemes.com">More than (just) Themes</a>. Designed by <a href="http://www.kaolti.com/">Zsolt Kacso</a></p>
+        <p id="powered" >Powered by <a href="http://www.rrrui.com">RRRUI.COM</a></p>
         </div>
     </div>
     <!-- EOF: #credits -->
